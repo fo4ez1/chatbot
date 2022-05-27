1a09bf503b5edd2ed988ffe3a5e86334dea609b4
fo4ez1 <90087430+fo4ez1@users.noreply.github.com>
2022-05-27 17:15:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+$(function () {
+  const phrases = [
+    "Наш менеджер перезвонит Вам в ближайшее время!",
+    "Уточнить детали можно по телефону 123456789",
+    "Оставайтесь на связи!",
+    "Сегодня прекрасная погода!",
+    "С Вами очень приятно общаться!",
+  ];
+  const hello = "Привет, я бот Вася!";
+  const goodbye = "До свидания!";
+  const byeee = "Goodbye!";
+  const bye = "Пока!";
+  const chao = "Chao!";
+ 
+
+  function rPhrases() {
+    return phrases[Math.floor(Math.random() * phrases.length)];
+  }
+
+  $("h1").css("color", "red");
+  $("#answers").append(`<div class="bot_answ">${hello}</div>`);
+
+  $("#chatbot").click(function () {
+    $(this).toggleClass("show");
+  });
+  $("#answers").click(function () {
+    return false;
+  });
+  $("#question").click(function () {
+    return false;
+  });
+  $("#ok").click(function () {
+    let q = $("#question").val();
+    $("#question").val("");
+    //alert(q);
+    if (q.trim() != "") {
+      $("#answers").append(`<div class="human_answ">${q.trim()}</div>`);
+      //q = q.toLowerCase();
+      let byeLowerCase = bye.substring(0, bye.length - 1).toLowerCase();
+      let goodbyeLowerCase = goodbye
+        .substring(0, goodbye.length - 1)
+        .toLowerCase();
+       let byeeLowerCase = byeee.substring(0, byeee.length - 1).toLowerCase();
+      setTimeout(function () {
+        if (q.toLowerCase().search(byeLowerCase) != -1) {
+          $("#answers").append(`<div class="bot_answ">${bye}</div>`);
+        } else if (q.toLowerCase().search(goodbyeLowerCase) != -1) {
+          $("#answers").append(`<div class="bot_answ">${goodbye}</div>`);
+        } else if (q.toLowerCase().search(byeeLowerCase) != -1) {
+          $("#answers").append(`<div class="bot_answ">${chao}</div>`);
+        } else if (
+          q.toLowerCase().search("привет") != -1 ||
+          q.toLowerCase().search("здравствуйте") != -1
+        ) {
+          $("#answers").append(`<div class="bot_answ" ">${hello}</div>`);
+        } else {
+          $("#answers").append(`<div class="bot_answ">${rPhrases()}</div>`);
+        }
+        let chatbot = document.getElementById("chatbot");
+        $("#chatbot").animate(
+          {
+            scrollTop: chatbot.scrollHeight - chatbot.clientHeight,
+          },
+          500
+        );
+      }, 1000);
+    }
+
+    return false;
+  });
+  $("#question").keypress("keyup", function (event) {
+    if (event.keyCode == 13) {
+      $("#ok").click();
+      return false;
+    }
+  });
+});
